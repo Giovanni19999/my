05369ab213f1b9aba40a19df7b2d52cb4ecc33f1
Caliendo Julien <caliendo@hotmail.fr>
2021-02-23 15:36:22 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <title>[ENI Ecole Informatique] Cours PHP Semaine 1</title>
+        <link href="style/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Cours PHP Semaine 1</h1>
+        <h2>Les bases du langage</h2>
+        <ol>
+            <li><a href="1-bases/helloworld/index.php">Helloworld</a></li>
+            <li><a href="1-bases/phpinfo/index.php">phpinfo</a></li>
+            <li><a href="1-bases/variablesConstantes/index.php">Les variables et constantes</a></li>
+            <li><a href="1-bases/tp1/index.php">TP 1 : variables et constantes</a></li>
+            <li><a href="1-bases/tableaux/index.php">Les tableaux</a></li>
+            <li><a href="1-bases/conditionnelles/index.php">Les conditionnelles</a></li>
+            <li><a href="1-bases/boucles/index.php">Les boucles</a></li>
+            <li><a href="1-bases/include_require/index.php">L'inclusion de fichiers</a></li>
+            <li><a href="1-bases/tp2/index.php">TP 2 : tableaux</a></li>
+            <li><a href="1-bases/fonctionsUtiles/index.php">Les fonctions utiles</a></li>
+            <li><a href="1-bases/tp3/index.php">TP 3 : fonctions utiles</a></li>
+            <li><a href="1-bases/fonctions/index.php">Les fonctions</a></li>
+            <li><a href="1-bases/tp4/index.php">TP 4 : fonctions</a></li>
+            <li><a href="1-bases/expressionsRationnelles/index.php">Les expressions rationnelles</a></li>
+            <li><a href="1-bases/tp5/index.php">TP 5 : expressions rationnelles</a></li>
+        </ol>
+        <h2>La programmation orientée objet</h2>
+        <ol>
+            <li><a href="2-poo/creationEtUtilisationDUneClasse/index.php">la création et utilisation d'une classe</a></li>
+            <li><a href="2-poo/heritage/index.php">L'héritage</a></li>
+            <li><a href="2-poo/static/index.php">Les attributs et méthodes de classe</a></li>
+            <li><a href="2-poo/classeAbstraite/index.php">Les classes abstraites</a></li>
+            <li><a href="2-poo/tp1/index.php">TP 1 : classes</a></li>
+            <li><a href="2-poo/interface/index.php">Les interfaces</a></li>
+            <li><a href="2-poo/exceptions/index.php">Les exceptions</a></li>
+            <li><a href="2-poo/namespace/index.php">Les espaces de nom (namespace)</a></li>
+        </ol>
+    </body>
+</html>
